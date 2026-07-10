82e091c9a15dfdd91ab1e565bca672f2525cf65b
cecilia-wsk <cecilia.wielonsky@gmail.com>
2026-07-10 14:20:18 +0200

    crab particules red on mobile

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,5 +1,13 @@
+uniform vec4 uResolution;
+
 void main() {
   vec2 c = gl_PointCoord - vec2(0.5);
   if (length(c) > 0.5) discard;
-  gl_FragColor = vec4(1.0, 1., 1., 0.6);
+
+  float isMobile = step(uResolution.x, 768.0);
+
+  vec4 desktopColor = vec4(1.0, 1.0, 1.0, 0.6);
+  vec4 mobileColor  = vec4(1.0, 0.0, 0.0, 0.4);
+
+  gl_FragColor = mix(desktopColor, mobileColor, isMobile);
 }
